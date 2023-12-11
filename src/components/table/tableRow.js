c11401671366ea164f6635d3219a83354ced7b3b
Gabriel Matos Martins <128993484+gabrielmatosmartins@users.noreply.github.com>
2023-12-11 10:47:36 -0300

    Delete src directory

--- src/components/table/tableRow.js
+++
@@ -1,9 +0,0 @@
-export function TableRow(props){
-    return(
-        <tr>
-            <td>{props.data1}</td>
-            <td>{props.data2}</td>
-            <td>{props.data3}</td>
-        </tr>
-    );
-}
